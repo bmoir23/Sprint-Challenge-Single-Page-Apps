0d307dc462d0b15c917095110e1228b923af7636
Brian Moir <48401267+bmoir23@users.noreply.github.com>
2019-10-11 23:09:22 -0400

    .

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,31 +1,56 @@
+import React, { useState, useEffect} from 'react';
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-import React, { useState } from "react";
-import { Form, FormGroup, Label, Input} from "reactstrap";
+const Search = ({ values, errors, touched, status }) => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    if (status) {
+      setUser([...user, status]);
+    }
+  }, [status]);
 
-export default function SearchForm ({ onSearch }) {
-  const [search, setSearch] = useState({ name: "" });
+  return (
+    <div className="user-form">
+      <Form>
+        <Field type="text" name="name" placeholder="Place name here" />
+        {touched.name && errors.name && (
+          <p className="error">{errors.name}</p>
+        )}    
 
-
-const handleInputChange = event => {
-  setSearch({ ...search, name: event.target.value.substr(0, 20)});
+        <button type="submit">Search </button>
+      </Form>
+      {user.map(user => (
+        <ul key={user.id}>
+          <li>Name: {user.name}</li>
+        </ul>
+      ))}
+    </div>
+  );
 };
-return (
-  <Form onSubmit={event => onSearch(event, search )}>
-    <FormGroup className="SearchBar">
-      <Label for="exampleSearch">Search</Label>
-      <Input
-          className="search-bar"
-          type="search"
-          name="name"
-          id="exampleSearch"
-          placeholder="Search by Name"
-          onChange={handleInputChange}
-          value={search.name}
-          />
-    </FormGroup>
-  </Form>
-)
-}
+const FormikUserForm = withFormik({
+  mapPropsToValues({ name }) {
+    return {
+      name: name || ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+  }),
+  handleSubmit(values, { setStatus, resetForm }) {
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        console.log(res);
+        setStatus(res.data.result);
+        resetForm();
+      })
+      .catch(err => console.log(err.res));
+  }
+})(Search);
+console.log("This is the HOC", FormikUserForm);
+export default FormikUserForm;
 
 
 
